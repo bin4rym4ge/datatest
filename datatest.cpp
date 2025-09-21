745c231155b1c0ebad8fa06c4a01aa3cba73451f
bin4rym4ge <bin4rym4ge@users.noreply.github.com>
2025-09-20 20:42:43 -0700

    Update datatest.cpp
    format

--- datatest.cpp
+++ datatest.cpp
@@ -6,62 +6,62 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
-    // vector<string> in;
-    string in;
-    string temp;
-    string regstr;
+	string in;
+	string temp;
+	string regstr;
 
 
-    // stdin to string
-    for( ; getline(cin, temp); in = in + temp);
+	// stdin to string
+	for( ; getline(cin, temp); in = in + temp);
 
 
-    // Read regex file
-    // Get Location of file from first command line arg
-    ifstream regfile((string)argv[1]);
+	// Read regex file
+	// Get Location of file from first command line arg
+	ifstream regfile((string)argv[1]);
 
 
-    // assign regex file to string regstr
-    while(getline(regfile, temp)) regstr = regstr + temp;
+	// assign regex file to string regstr
+	while(getline(regfile, temp)) regstr = regstr + temp;
 
 
-    // Create a regex object from regstr
-    regex r(regstr);
-    smatch sm;
+	// Create a regex object from regstr
+	regex r(regstr);
+	smatch sm;
 
-    // return all \1 submatches as comma seperated list items
-    // reqireds all regex to end in (.*) to continue matching
-    // think of this as using /g global tag
+	// return all \1 submatches as comma seperated list items
+	// reqireds all regex to end in (.*) to continue matching
+	// think of this as using /g global tag
 
-    do{
+	do{
 
-	regex_search(in, sm, r);
-
-	if( (sm[2] != "") || (sm[2] != "\0") ){
-	    cout << sm[1] << ", ";
-	} else {
-	    cout << sm[1] << endl;
-	}
-
-	in = sm[2];
+		regex_search(in, sm, r);
 
 
-    } while( (in != "") || (in != "\0") );
+		if( (sm[2] != "") || (sm[2] != "\0") ){
+			cout << sm[1] << ", ";
+		} else {
+			cout << sm[1] << endl;
+		}
 
-    /*
-    while( (in != "") || (in != "\0") ){
+		in = sm[2];
 
-	regex_search(in, sm, r);
 
-	cout << sm[1] << ", ";
+	} while( (in != "") || (in != "\0") );
 
-	in = sm[2];
+	/*
+	   while( (in != "") || (in != "\0") ){
 
-    }
-    */
+	   regex_search(in, sm, r);
 
-    cout << endl;
+	   cout << sm[1] << ", ";
 
-    return 0;
+	   in = sm[2];
+
+	   }
+	   */
+
+	cout << endl;
+
+	return 0;
 }
 
