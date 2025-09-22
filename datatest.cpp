2b42c8ba3f27ef1a6258e057e494d600854e0179
bin4rym4ge <bin4rym4ge@users.noreply.github.com>
2025-09-21 22:57:19 -0700

    Update datatest.cpp
    more comments and complete arg functionality.

--- datatest.cpp
+++ datatest.cpp
@@ -4,16 +4,36 @@
 
 using namespace std;
 
+void helpscrn();
+
 int main(int argc, char* argv[]){
+
+	// display help screen in first arg is -h or --help
+	if( ((string)argv[1] == "-h") || ((string)argv[1] == "--help") ){
+
+		// display help screen
+		helpscrn();
+
+		return 0;
+	}
 
 	string in;
 	string temp;
 	string regstr;
 
+	// if no second arg
+	if(!argv[2]){
+		// stdin to string in
+		for( ; getline(cin, temp); in = in + temp) ;
+	} else {
+		// file to string in
+		
+		ifstream infile((string)argv[2]);
 
-	// stdin to string
-	for( ; getline(cin, temp); in = in + temp);
+		for( ; getline(infile, temp); in = in + temp) ;
 
+		infile.close();
+	}
 
 	// Read regex file
 	// Get Location of file from first command line arg
@@ -34,9 +54,11 @@ int main(int argc, char* argv[]){
 
 	do{
 
+		// do a regex search and save the resulting
+		// submatches to std::smatch sm
 		regex_search(in, sm, r);
 
-
+		// revise later so sudo global not required
 		if( (sm[2] != "") || (sm[2] != "\0") ){
 			cout << sm[1] << ", ";
 		} else {
@@ -48,20 +70,25 @@ int main(int argc, char* argv[]){
 
 	} while( (in != "") || (in != "\0") );
 
-	/*
-	   while( (in != "") || (in != "\0") ){
-
-	   regex_search(in, sm, r);
-
-	   cout << sm[1] << ", ";
-
-	   in = sm[2];
-
-	   }
-	   */
-
 	cout << endl;
 
 	return 0;
 }
 
+// help screen
+void helpscrn(){
+
+	cout << "\t+=======================================================+" << endl;
+	cout << "\t|\t\t\t\t\t\t\t|\n";
+	cout << "\t|\t--help (-h)\t\t\t\t\t|\n";
+
+	cout << "\t|\t\t\t\t\t\t\t|\n";
+	cout << "\t|\t    test /path/to/regex path/to/file\t\t|\n";
+
+	cout << "\t|\t\t\t\t\t\t\t|\n";
+	cout << "\t|\t    cat input | test /path/to/regex\t\t|\n";
+
+	cout << "\t|\t\t\t\t\t\t\t|\n";
+	cout << "\t+=======================================================+" << endl;
+
+}
